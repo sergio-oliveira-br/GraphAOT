9057534bd101309a22759cc6be8839c643bb3a81
Sergio <oliveira-sergio@outlook.com>
2026-04-02 20:56:22 +0100

    Create s3_storage.py
    act:
    -> contract implemented
    -> this upload files to s3

---
+++ src/providers/s3_storage.py
@@ -0,0 +1,25 @@
+# src/providers/s3_storage.py
+
+import boto3
+import logging
+from src.interfaces import IStorage
+
+class S3Storage(IStorage):
+    def __init__(self, bucket_name: str):
+        self.bucket_name = bucket_name
+        self.s3_client = boto3.client('s3')
+        self.logger = logging.getLogger(__name__)
+
+    def upload_file(self, local_path: str, remote_key: str, bucket_type: str) -> bool:
+        # There are two folders in the S3, one for POM and the other for BOM
+        prefix = "analysis" if bucket_type == "analysis" else "audit-logs"
+        full_key = f"{prefix}/{remote_key}"
+
+        try:
+            self.s3_client.upload_file(local_path, self.bucket_name, full_key)
+            self.logger.info(f"Success: {local_path} -> s3://{self.bucket_name}/{full_key}")
+            return True
+
+        except Exception as e:
+            self.logger.error(f"Error uploading {local_path}: {str(e)}")
+            return False
