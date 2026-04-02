3b7db0b8af2655c9f93b0497c823dd8e7a30ba72
Sergio <oliveira-sergio@outlook.com>
2026-04-02 20:58:36 +0100

    Update interfaces.py

--- src/interfaces.py
+++ src/interfaces.py
@@ -6,3 +6,11 @@ class IStorage(ABC):
     @abstractmethod
     def upload_file(self, local_path: str, remote_key: str, bucket_type: str) -> bool:
         pass
+
+class IVersionControl(ABC):
+    @abstractmethod
+    def clone(self, url: str, target_path: str) -> bool:
+        """
+        Clone a repository for the destination path.
+        """
+        pass
