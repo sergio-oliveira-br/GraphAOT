aeb5aeeff62ab7f82339c0a81b45e86d258d070f
Sergio <oliveira-sergio@outlook.com>
2026-04-03 07:56:03 +0100

    Update interfaces.py
    just comments

--- src/interfaces.py
+++ src/interfaces.py
@@ -2,6 +2,7 @@
 
 from abc import ABC, abstractmethod
 
+# Contract for storage
 class IStorage(ABC):
     @abstractmethod
     def upload_file(self, local_path: str, remote_key: str, bucket_type: str) -> bool:
