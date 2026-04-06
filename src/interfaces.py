569bc30bd80c71217ea28860446c674aa72f778e
Sergio <oliveira-sergio@outlook.com>
2026-04-06 10:23:56 +0100

    Update interfaces.py
    code refactored
    
    act:
    -> code moved to a new file

--- src/interfaces.py
+++ src/interfaces.py
@@ -4,17 +4,7 @@ from abc import ABC, abstractmethod
 import networkx as nx
 
 
-# Contract for the extraction of artifacts
-class IBuildTool(ABC):
-    @abstractmethod
-    def generate_bom(self, project_path: str) -> str:
-        """Generates the CycloneDX SBOM"""
-        pass
 
-    @abstractmethod
-    def generate_audit_data(self, project_path: str) -> str:
-        """Generates the POM"""
-        pass
 
 # Contract for the architecture of Graphs
 class GraphProvider(ABC):
