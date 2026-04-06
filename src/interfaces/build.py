55caec82cf4d42ada8b3f453cbbc9e1c963484c7
Sergio <oliveira-sergio@outlook.com>
2026-04-06 10:24:29 +0100

    Create build.py
    code refactored
    
    act:
    -> code moved from main interface

---
+++ src/interfaces/build.py
@@ -0,0 +1,15 @@
+# src/interfaces/build.py
+
+from abc import ABC, abstractmethod
+
+# Contract for the extraction of artifacts
+class IBuildTool(ABC):
+    @abstractmethod
+    def generate_bom(self, project_path: str) -> str:
+        """Generates the CycloneDX SBOM"""
+        pass
+
+    @abstractmethod
+    def generate_audit_data(self, project_path: str) -> str:
+        """Generates the POM"""
+        pass
