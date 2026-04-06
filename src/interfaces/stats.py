f0d679feb134d1de5e53d967eafd156d02a4691f
Sergio <oliveira-sergio@outlook.com>
2026-04-06 10:30:11 +0100

    Create stats.py
    code refactored
    
    act:
    -> code moved from main interface

---
+++ src/interfaces/stats.py
@@ -0,0 +1,14 @@
+# src/interfaces/stats.py
+
+from abc import ABC, abstractmethod
+
+# Statistics manager
+class StatsProvider(ABC):
+    @abstractmethod
+    def save_metrics(self, project_id: str, metrics: dict):
+        pass
+
+    @abstractmethod
+    def _initialize_storage(self):
+        """create file or table"""
+        pass
