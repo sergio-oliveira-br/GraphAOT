e29b23bd21f9c39da3ce49b4ee7b275b7e69e114
Sergio <oliveira-sergio@outlook.com>
2026-04-06 10:27:35 +0100

    Update interfaces.py
    code refactored
    
    act:
    -> code moved to a new file

--- src/interfaces.py
+++ src/interfaces.py
@@ -1,22 +1,12 @@
 # src/interfaces.py
 
 from abc import ABC, abstractmethod
-import networkx as nx
 
 
 
 
-# Contract for the architecture of Graphs
-class GraphProvider(ABC):
-    @abstractmethod
-    def build_from_bom(self, bom_path: str) -> nx.DiGraph:
-        """Transforms a BOM into a graph"""
-        pass
 
-    @abstractmethod
-    def get_metrics(self, graph: nx.DiGraph) -> dict:
-        """Extract topological metrics graph - (SRQ1)"""
-        pass
+
 
 # Statistics manager
 class StatsProvider(ABC):
