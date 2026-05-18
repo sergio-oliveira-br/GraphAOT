cc1c1b03e7e7c63fd696fe725be89d579fdb3229
Sergio <oliveira-sergio@outlook.com>
2026-05-18 19:04:57 +0100

    Create versions.tf
    act:
    -> min version sets of terraform

---
+++ terraform/versions.tf
@@ -0,0 +1,13 @@
+# terraform/versions.tf
+
+# Sets the minimum version of Terraform and providers.tf
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
